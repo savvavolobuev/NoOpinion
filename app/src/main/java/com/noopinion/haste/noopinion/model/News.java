1d28360a7457a5d11fdb99e553f2d90c553da436
Ivan Gusev <ivan_gusev1@epam.com>
2015-11-29 18:08:28 +0300

    Added dependencies for libraries.

--- app/src/main/java/com/noopinion/haste/noopinion/model/News.java
+++ app/src/main/java/com/noopinion/haste/noopinion/model/News.java
@@ -1,7 +1,42 @@
 package com.noopinion.haste.noopinion.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.hannesdorfmann.parcelableplease.annotation.ParcelablePlease;
+
 /**
  * Created by Ivan Gusev on 29.11.2015.
  */
-public final class News {
+@ParcelablePlease
+public final class News implements Parcelable {
+
+    int    mId;
+    String mText;
+    String mLink;
+    String mImage;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(final Parcel dest, final int flags) {
+        NewsParcelablePlease.writeToParcel(this, dest, flags);
+    }
+
+    public static final Creator<News> CREATOR = new Creator<News>() {
+        @Override
+        public News createFromParcel(final Parcel source) {
+            final News news = new News();
+            NewsParcelablePlease.readFromParcel(news, source);
+            return news;
+        }
+
+        @Override
+        public News[] newArray(final int size) {
+            return new News[size];
+        }
+    };
 }
--- app/src/main/java/com/noopinion/haste/noopinion/provider/NewsProvider.java
+++ app/src/main/java/com/noopinion/haste/noopinion/provider/NewsProvider.java
@@ -2,6 +2,7 @@ package com.noopinion.haste.noopinion.provider;
 
 import com.noopinion.haste.noopinion.model.News;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,4 +10,13 @@ import java.util.List;
  */
 public interface NewsProvider {
     List<News> getNews();
+}
+
+final class MockNewsProvider implements NewsProvider {
+
+    @Override
+    public List<News> getNews() {
+        final List<News> news = new ArrayList<>();
+        return null;
+    }
 }
